5290f1f530c66226b59bcc9cab0d8762c29b5931
shaunte99 <ksmatsetela99@gmail.com>
2024-11-14 21:37:39 +0200

    Add files via upload

---
+++ Journal.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<JournalEntry> entries = new List<JournalEntry>();
+    private List<string> prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
+
+    public void AddEntry()
+    {
+        string prompt = GetRandomPrompt();
+        Console.WriteLine(prompt);
+        string response = Console.ReadLine();
+        entries.Add(new JournalEntry(prompt, response));
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (var entry in entries)
+            {
+                writer.WriteLine(entry);
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        entries.Clear();
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            var parts = line.Split("~|~");
+            if (parts.Length == 3)
+            {
+                entries.Add(new JournalEntry(parts[1], parts[2]) { Date = parts[0] });
+            }
+        }
+    }
+
+    private string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(prompts.Count);
+        return prompts[index];
+    }
+}
